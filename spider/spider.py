66bf29e406c3c9b877d8f669b0cf5f13be0fe404
Franksong <spf.doudou@gmail.com>
2013-01-11 20:02:53 +0800

    up

---
+++ spider/spider.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+# Filename: spider.py
+# Author: Frank
+# Usage: python spider.py
+# deep = 2
+
+from thread_pool import *
+from sgmllib import SGMLParser
+import os, sys
+import urllib2
+import re
+import sqlite3
+import string
+import logging
+import chardet
+#sys.setdefaultencoding('utf-8')
+
+class GetUrls(SGMLParser):
+    def reset(self):
+        self.urls = []
+        SGMLParser.reset(self)
+    def start_a(self, attrs):
+        """
+        
+        Arguments:
+        - `self`:
+        - `attrs`:
+        """
+        url = [value for key, value in attrs if key == 'href']
+        if url:
+            self.urls.extend(url)
+
+def do_get_content(url_link):
+    logging.info('Begin: %s', url_link)
+    try:
+        fd = urllib2.urlopen(url_link)
+    except:
+        logging.warning('wrong url: %s', url_link)
+        return ''
+    content = fd.read()
+    logging.debug('encodeing_info: %s', chardet.detect(content))
+    #content = content.decode('gb2312').encode('utf-8')
+    logging.info('Finished: %s', url_link)
+    return content
+
+def html_text(html):
+    """
+    
+    Arguments:
+    - `html`:
+    """
+    html = string.replace(html, '“', "'")
+    html = string.replace(html, '"', "'")
+    html = string.replace(html, '；', ";")
+    html = string.replace(html, '/', '&quot')
+    html = string.replace(html, '%', '&percent')
+    return html
+
+def save_parser(data, sql):
+    """
+    
+    Arguments:
+
+    - `data`:
+    """
+    #filename = '/home/frank/mywork/html/qq.html'
+    parser = GetUrls()
+    try:
+        parser.feed(data)
+    except:
+        logging.warning('can not parser HTML!')
+    #f = open(filename, 'a')
+    #f.write(data)
+    #f.write('Finished')
+    #f.close()
+    #text = html_text(data)
+    text = (data,)
+    #statement = 'INSERT INTO html VALUES (?)', text
+    try:
+        sql.execute('INSERT INTO html (data) VALUES (?)',(data,))
+    except:
+        logging.error('INSERT html data error!')
+        print 'INSERT ERROR!!!'
+        pass
+    return parser.urls
+    
+
+def do_get_con(deep, url_list, sql):
+    get_urls = []
+    tpm = ThreadPoolManager(argv_dict['--thread'])
+    for url in url_list:
+        tpm.add_job(do_get_content, url)
+
+    tpm.wait_for_complete()
+    while tpm.resultQueue.qsize():
+        data = tpm.resultQueue.get()
+        get_urls.extend(save_parser(data, sql))
+    deep = deep -1
+    if deep <= 0:
+        return 0
+    result_urls = []
+    for url in get_urls:
+        if url not in finished_urls:
+            result_urls.append(url)
+            finished_urls.append(url)
+    return do_get_con(deep, result_urls, sql)
+
+argv_dict = {
+    '-u': 'http://www.qq.com',
+    '-d': 3,
+    '-f': 'spider.log',
+    '-l': '4',
+    '--thread': 10,
+    '--dbfile': '/home/frank/mywork/html/spider.db',
+    '--key': False,
+    '--testself': False
+    }
+#log_levels = ['logging.CRITICAL', 'logging.ERROR', 'logging.WARNING', \
+#                  'logging.INFO', 'logging.DEBUG']
+log_levels = ['50', '40', '30', '20', '10']
+argv_list = sys.argv[1:]
+
+print argv_list # test for output argv
+if len(argv_list) % 2 != 0:
+    if '--testself' in argv_list:
+        argv_list.remove('--testself')
+        argv_dict['--testself'] = True
+    else:
+        print 'wrong argv.'
+        sys.exit()
+
+while len(argv_list):
+    value = argv_list.pop()
+    key = argv_list.pop()
+    if not argv_dict.has_key(key):
+        print 'wrong argv!'
+        sys.exit()
+    argv_dict[key] = value
+argv_dict['-d'] = int(argv_dict['-d'])
+argv_dict['-l'] = int(re.search('[0-9]', argv_dict['-l']).group(0))
+argv_dict['--thread'] = int(argv_dict['--thread'])
+
+
+logfile = argv_dict['-f']
+log_level = log_levels[argv_dict['-l']-1]
+logging.basicConfig(filename = logfile, level = int(log_level))
+logging.info('Started')
+
+url_list = []
+url_list.append(argv_dict['-u'])
+logging.debug(url_list)
+
+global finished_urls
+finished_urls = []
+finished_urls.extend(url_list)
+
+conn = sqlite3.connect(argv_dict['--dbfile'])
+cur_sql = conn.cursor()
+cur_sql.execute('CREATE TABLE html (data BLOB)')
+do_get_con(argv_dict['-d'], url_list, cur_sql)
+conn.commit()
+conn.close()
+logging.debug(argv_dict)
+logging.info('Finished\n')
+
+print 'Done'
---
+++ spider/thread_pool.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# Author: Frank
+
+import Queue, threading
+from threading import Thread
+import time
+import logging
+
+class ThreadWork(Thread):
+    """
+    """
+    timeout = 1
+    def __init__(self, workQueue, resultQueue, **argv):
+        """
+        
+        Arguments:
+        - `workQueue`:
+        - `resultQueue`:
+        - `**argv`:
+        """
+        Thread.__init__(self, **argv)
+        
+        self.setDaemon(True)
+        self._workQueue = workQueue
+        self._resultQueue = resultQueue
+        self.start()
+
+    def run(self):
+        """
+        
+        Arguments:
+        - `self`:
+        """
+        while True:
+            try:
+                callfunc, args, argv = self._workQueue.get(timeout = \
+                                                               ThreadWork.timeout)
+                result = callfunc(*args, **argv)
+                self._resultQueue.put(result)
+            except Queue.Empty:
+                logging.info('threadpool work queue empty!')
+                break
+            except:
+                logging.error('ThreadWork: %s', sys.exc_info())
+                raise
+
+
+class ThreadPoolManager:
+    """
+    """
+    
+    def __init__(self, num_of_threads = 10, timeout = 1):
+        """
+        """
+        self.workQueue = Queue.Queue(0)
+        self.resultQueue = Queue.Queue(0)
+        self.threads = []
+        self.timeout = timeout
+        self._createThreads(num_of_threads)
+
+    def _createThreads(self, num_of_threads):
+        """
+        
+        Arguments:
+        - `self`:
+        - `num_of_threads`:
+        """
+        for i in range(num_of_threads):
+            thread = ThreadWork(self.workQueue, self.resultQueue)
+            self.threads.append(thread)
+
+    def wait_for_complete(self):
+        """
+        
+        Arguments:
+        - `self`:
+        """
+        while len(self.threads):
+            thread = self.threads.pop()
+            thread.join()
+            if thread.isAlive() and not self.workQueue.empty():
+                self.threads.append(thread)
+        logging.info('All threads are completed.')
+
+    def add_job(self, callfunc, *args, **argv):
+        """
+        
+        Arguments:
+        - `self`:
+        - `callfunc`:
+        - `*args`:
+        - `**argv`:
+        """
+        self.workQueue.put( (callfunc, args, argv) )
+        logging.info('threadpool addjob: %s, %s', args, argv)
